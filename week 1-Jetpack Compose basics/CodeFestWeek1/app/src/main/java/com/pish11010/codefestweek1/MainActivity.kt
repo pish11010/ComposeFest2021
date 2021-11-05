907840d2e43e8d842dd38853f99e3e14d954e8ff
JaeHeung Choi <pish11010@gmail.com>
2021-11-05 20:31:44 +0900

    [basics-11] Animating your list

--- week 1-Jetpack Compose basics/CodeFestWeek1/app/src/main/java/com/pish11010/codefestweek1/MainActivity.kt
+++ week 1-Jetpack Compose basics/CodeFestWeek1/app/src/main/java/com/pish11010/codefestweek1/MainActivity.kt
@@ -3,6 +3,9 @@ package com.pish11010.codefestweek1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -53,7 +56,7 @@ fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
   LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
     items(items = names) { name ->
       Greeting(name = name)
@@ -63,10 +66,15 @@ private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
 
 @Composable
 private fun Greeting(name: String) {
+  var expanded by remember { mutableStateOf(false) }
 
-  val expanded = remember { mutableStateOf(false) }
-
-  val extraPadding = if (expanded.value) 48.dp else 0.dp
+  val extraPadding by animateDpAsState(
+    if (expanded) 48.dp else 0.dp,
+    animationSpec = spring(
+      dampingRatio = Spring.DampingRatioMediumBouncy,
+      stiffness = Spring.StiffnessLow
+    )
+  )
 
   Surface(
     color = MaterialTheme.colors.primary,
@@ -75,15 +83,15 @@ private fun Greeting(name: String) {
     Row(modifier = Modifier.padding(24.dp)) {
       Column(modifier = Modifier
         .weight(1f)
-        .padding(bottom = extraPadding)
+        .padding(bottom = extraPadding.coerceAtLeast(0.dp))
       ) {
         Text(text = "Hello, ")
         Text(text = name)
       }
       OutlinedButton(
-        onClick = { expanded.value = !expanded.value }
+        onClick = { expanded = !expanded }
       ) {
-        Text(if (expanded.value) "Show less" else "Show more")
+        Text(if (expanded) "Show less" else "Show more")
       }
     }
   }
