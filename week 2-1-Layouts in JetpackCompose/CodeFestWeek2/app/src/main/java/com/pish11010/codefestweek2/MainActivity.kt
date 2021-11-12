fce4c1380c94c2aa41bef7916ac038f055595d03
JaeHeung Choi <pish11010@gmail.com>
2021-11-12 14:40:56 +0900

    [layout] 3. Modifiers

--- week 2-1-Layouts in JetpackCompose/CodeFestWeek2/app/src/main/java/com/pish11010/codefestweek2/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/CodeFestWeek2/app/src/main/java/com/pish11010/codefestweek2/MainActivity.kt
@@ -3,11 +3,27 @@ package com.pish11010.codefestweek2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pish11010.codefestweek2.ui.theme.CodeFestWeek2Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +33,7 @@ class MainActivity : ComponentActivity() {
       CodeFestWeek2Theme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
+//          Greeting("Android")
         }
       }
     }
@@ -25,14 +41,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun PhotographerCard(modifier: Modifier = Modifier) {
+  Row(modifier
+    .padding(8.dp)
+    .clip(RoundedCornerShape(4.dp))
+    .background(MaterialTheme.colors.surface)
+    .clickable(onClick = { /* Ignoring onClick */ })
+    .padding(16.dp)
+  ) {
+    Surface(
+      modifier = Modifier.size(50.dp),
+      shape = CircleShape,
+      color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+    ) {
+      // Image goes here
+    }
+    Column(
+      modifier = Modifier
+        .padding(start = 8.dp)
+        .align(Alignment.CenterVertically)
+    ) {
+      Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+      CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Text("3 minutes ago", style = MaterialTheme.typography.body2)
+      }
+    }
+  }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun PhotographerCardPreview() {
   CodeFestWeek2Theme {
-    Greeting("Android")
+    PhotographerCard()
   }
 }
