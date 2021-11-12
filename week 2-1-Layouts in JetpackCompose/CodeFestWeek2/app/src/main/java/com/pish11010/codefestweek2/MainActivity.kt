c6893ca605a2fe52254af0dd3a74acd437842e2c
JaeHeung Choi <pish11010@gmail.com>
2021-11-12 14:53:10 +0900

    [layout] 5. Material Components

--- week 2-1-Layouts in JetpackCompose/CodeFestWeek2/app/src/main/java/com/pish11010/codefestweek2/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/CodeFestWeek2/app/src/main/java/com/pish11010/codefestweek2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pish11010.codefestweek2
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -11,11 +12,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -31,12 +39,45 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       CodeFestWeek2Theme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-//          Greeting("Android")
-        }
+        LayoutsCodelab()
       }
     }
+  }
+}
+
+@Composable
+fun LayoutsCodelab() {
+  Scaffold(
+    topBar = {
+      TopAppBar(
+        title = {
+          Text(text = "LayoutsCodelab")
+        },
+        actions = {
+          IconButton(onClick = { /* doSomething() */ }) {
+            Icon(Icons.Filled.Favorite, contentDescription = null)
+          }
+        }
+      )
+    }
+  ) { innerPadding ->
+    BodyContent(Modifier.padding(innerPadding))
+  }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+  Column(modifier = modifier.padding(8.dp)) {
+    Text(text = "Hi there!")
+    Text(text = "Thanks for going through the Layouts codelab")
+  }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+  CodeFestWeek2Theme {
+    LayoutsCodelab()
   }
 }
 
