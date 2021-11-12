9580b41efb25231d4182d38e3ad506c3725c2c08
JaeHeung Choi <pish11010@gmail.com>
2021-11-12 20:18:42 +0900

    [layout] 7. Create your custom layout

--- week 2-1-Layouts in JetpackCompose/CodeFestWeek2/app/src/main/java/com/pish11010/codefestweek2/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/CodeFestWeek2/app/src/main/java/com/pish11010/codefestweek2/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.pish11010.codefestweek2
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -35,8 +34,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.pish11010.codefestweek2.ui.theme.CodeFestWeek2Theme
@@ -75,9 +79,11 @@ fun LayoutsCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-  Column(modifier = modifier.padding(8.dp)) {
-    Text(text = "Hi there!")
-    Text(text = "Thanks for going through the Layouts codelab")
+  MyOwnColumn(modifier.padding(16.dp)) {
+    Text("MyOwnColumn")
+    Text("places items")
+    Text("vertically.")
+    Text("We've done it by hand!")
   }
 }
 
@@ -176,5 +182,58 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
 fun PhotographerCardPreview() {
   CodeFestWeek2Theme {
     PhotographerCard()
+  }
+}
+
+fun Modifier.firstBaselineToTop(
+  firstBaselineToTop: Dp,
+) = this.then(
+  layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+      // Where the composable gets placed
+      placeable.placeRelative(0, placeableY)
+    }
+  }
+)
+
+@Composable
+fun MyOwnColumn(
+  modifier: Modifier = Modifier,
+  content: @Composable () -> Unit
+) {
+  Layout(
+    modifier = modifier,
+    content = content
+  ) { measurables, constraints ->
+    // Don't constrain child views further, measure them with given constraints
+    // List of measured children
+    val placeables = measurables.map { measurable ->
+      // Measure each child
+      measurable.measure(constraints)
+    }
+
+    // Track the y co-ord we have placed children up to
+    var yPosition = 0
+
+    // Set the size of the layout as big as it can
+    layout(constraints.maxWidth, constraints.maxHeight) {
+      // Place children in the parent layout
+      placeables.forEach { placeable ->
+        // Position item on the screen
+        placeable.placeRelative(x = 0, y = yPosition)
+
+        // Record the y co-ord placed up to
+        yPosition += placeable.height
+      }
+    }
   }
 }
