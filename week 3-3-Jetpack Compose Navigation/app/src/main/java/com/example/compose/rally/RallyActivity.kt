ad385ad154ed7528caf9972bd76ba05cbeb6a29b
JaeHeung Choi <pish11010@gmail.com>
2021-12-03 18:05:12 +0900

    [navigation] 3. Migrate to Navigation

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,16 +19,24 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.rally.data.UserData
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewBody
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -36,35 +44,52 @@ import com.example.compose.rally.ui.theme.RallyTheme
  * https://material.io/design/material-studies/rally.html
  */
 class RallyActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            RallyApp()
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      RallyApp()
     }
+  }
 }
 
 @Composable
 fun RallyApp() {
-    RallyTheme {
-        val allScreens = RallyScreen.values().toList()
-        var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
-        Scaffold(
-            topBar = {
-                RallyTabRow(
-                    allScreens = allScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
-                    currentScreen = currentScreen
-                )
-            }
-        ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.content(
-                    onScreenChange = { screen ->
-                        currentScreen = RallyScreen.valueOf(screen)
-                    }
-                )
-            }
+  RallyTheme {
+    val allScreens = RallyScreen.values().toList()
+    val navController = rememberNavController()
+    val backstackEntry = navController.currentBackStackEntryAsState()
+    val currentScreen = RallyScreen.fromRoute(
+      backstackEntry.value?.destination?.route
+    )
+    Scaffold(
+      topBar = {
+        RallyTabRow(
+          allScreens = allScreens,
+          onTabSelected = { screen ->
+            navController.navigate(screen.name)
+          },
+          currentScreen = currentScreen,
+        )
+      }
+    ) { innerPadding ->
+      NavHost(
+        navController = navController,
+        startDestination = RallyScreen.Overview.name,
+        modifier = Modifier.padding(innerPadding)
+      ) {
+        composable(RallyScreen.Overview.name) {
+          OverviewBody(
+            onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
+            onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) },
+          )
         }
+        composable(RallyScreen.Accounts.name) {
+          AccountsBody(accounts = UserData.accounts)
+        }
+        composable(RallyScreen.Bills.name) {
+          BillsBody(bills = UserData.bills)
+        }
+      }
     }
+  }
 }
