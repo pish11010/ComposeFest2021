ae7d3e2257b90e8a1d96652057ddf0ca30e8809e
JaeHeung Choi <pish11010@gmail.com>
2021-12-07 18:00:10 +0900

    [migrating] 11. Interop theming

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,7 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.foundation.layout.Column
@@ -38,6 +39,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -65,7 +67,7 @@ fun PlantDetailContent(plant: Plant) {
 @Composable
 private fun PlantDetailContentPreview() {
   val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
-  MaterialTheme {
+  MdcTheme {
     PlantDetailContent(plant)
   }
 }
@@ -85,7 +87,7 @@ private fun PlantName(name: String) {
 @Preview
 @Composable
 private fun PlantNamePreview() {
-  MaterialTheme {
+  MdcTheme {
     PlantName("Apple")
   }
 }
@@ -120,7 +122,7 @@ private fun PlantWatering(wateringInterval: Int) {
 @Preview
 @Composable
 private fun PlantWateringPreview() {
-  MaterialTheme {
+  MdcTheme {
     PlantWatering(7)
   }
 }
@@ -149,7 +151,16 @@ private fun PlantDescription(description: String) {
 @Preview
 @Composable
 private fun PlantDescriptionPreview() {
-  MaterialTheme {
+  MdcTheme {
     PlantDescription("HTML<br><br>description")
+  }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PlantDetailContentDarkPreview() {
+  val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+  MdcTheme {
+    PlantDetailContent(plant)
   }
 }
